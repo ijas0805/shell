189bf6531facd3cc5d56aabf350724bd88e032e8
IJAS AHAMMED A P <86339769+ijas0805@users.noreply.github.com>
2022-04-01 21:07:31 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-sudo rm -rf crawler
-sudo apt autoremove -y
-sudo apt install python3-pip -y
-sudo apt install redis-tools -y
-sudo apt install redis-server -y
-sudo git clone https://github.com/firedrak/crawler.git
-sudo pip install -r crawler/requirements.txt
+rm -rf crawler
+apt autoremove -y
+apt install python3-pip -y
+apt install redis-tools -y
+apt install redis-server -y
+git clone https://github.com/firedrak/crawler.git
+pip install -r crawler/requirements.txt
 # time python3 crawler/main.py $1 $2
 
